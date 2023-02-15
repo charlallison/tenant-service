920b9401e6e064bd063289066196f8deade52de2
Charles Allison <soprinye@gmail.com>
2023-02-15 19:04:19 +0100

    adds GetTenants feature

---
+++ lambda/GetTenants/index.ts
@@ -0,0 +1,22 @@
+import {formatJSONResponse, ValidatedEventAPIGatewayProxyEvent} from "../../src/libs/api-gateway";
+import {middyfy} from "../../src/libs/lambda";
+import schema from "../CreateTenant/schema";
+import {DynamoDBClient, ScanCommand} from "@aws-sdk/client-dynamodb";
+import {unmarshall} from "@aws-sdk/util-dynamodb";
+
+const dynamodbClient = new DynamoDBClient({
+  region: process.env.REGION
+})
+
+const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const result = await dynamodbClient.send(new ScanCommand({
+    TableName: process.env.TENANT_TABLE_NAME
+  }));
+
+  const tenants = result.Items.map(item => unmarshall(item));
+
+
+  return formatJSONResponse({tenants})
+}
+
+export const main = middyfy(handler);
