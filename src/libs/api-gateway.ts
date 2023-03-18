d2f81b14dcc065eee0ec259d68e14353ec6f8981
Charles Allison <soprinye@gmail.com>
2023-03-18 03:02:42 +0100

    Create type for pathParameter in from APIGateway proxy event

--- src/libs/api-gateway.ts
+++ src/libs/api-gateway.ts
@@ -2,11 +2,14 @@ import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-l
 import type { FromSchema } from "json-schema-to-ts";
 
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+type ValidatedAPIGatewayProxyEventPathParameters<S> = Omit<APIGatewayProxyEvent, 'pathParameters'> & { pathParameters: FromSchema<S> }
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+export type ValidatedEventAPIGatewayProxyEventPathParameters<S> = Handler<ValidatedAPIGatewayProxyEventPathParameters<S>, APIGatewayProxyResult>
+
+export const formatJSONResponse = (response: Record<string, unknown>, statusCode = 200, ) => {
   return {
-    statusCode: 200,
+    statusCode,
     body: JSON.stringify(response)
   }
 }
