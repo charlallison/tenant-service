baf64ab5909443cf36781a481fa4824ecddac15b
Charles Allison <soprinye@gmail.com>
2023-04-01 13:28:10 +0100

    create GetProperty function

---
+++ lambda/GetProperty/index.ts
@@ -0,0 +1,28 @@
+import {formatJSONResponse, ValidatedEventAPIGatewayProxyEvent} from "@libs/api-gateway";
+import schema from "./schema";
+import {middyfy} from "@libs/lambda";
+import {ddbClient} from "@libs/aws-client";
+import {GetItemCommand} from "@aws-sdk/client-dynamodb";
+import {marshall, unmarshall} from "@aws-sdk/util-dynamodb";
+import {NotFound} from "http-errors";
+
+const handler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const { id } = event.pathParameters;
+
+  const response = await ddbClient.send(new GetItemCommand({
+    TableName: process.env.TENANT_TABLE_NAME,
+    Key: marshall({id})
+  }));
+
+  if (!response.Item) {
+    const { message, statusCode } = new NotFound(`Property not found`)
+    return formatJSONResponse({message}, statusCode)
+  }
+
+  return formatJSONResponse({
+    message: `Property found`,
+    property: unmarshall(response.Item)
+  })
+}
+
+export const main = middyfy(handler, schema);
---
+++ lambda/GetProperty/schema.ts
@@ -0,0 +1,13 @@
+export default {
+  type: 'object',
+  required: ['pathParameters'],
+  properties: {
+    pathParameters: {
+      type: 'object',
+      required: ['id'],
+      properties: {
+        id: { type: 'string'}
+      }
+    }
+  }
+} as const
